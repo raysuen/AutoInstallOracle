b23a26107298ee075397156d1dd7cce8647c3f22
raysuen <braveray@163.com>
2023-05-22 16:40:00 +0800

    20230522

--- github.sh
+++ github.sh
@@ -2,5 +2,5 @@
 
 git add *
 git commit -m "`/Users/raysuen/raysuen/bin/rdate.py -f "%Y%m%d"`"
-git push -u origin "master"
+git push -u origin "main"
 #git remote add origin https://gitee.com/raysuen/autoinstalloracle.git
